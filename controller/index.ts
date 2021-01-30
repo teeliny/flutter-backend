8cd9d87d9c40ba3e259566d64e3fd76e9779e820
teeliny <teeliny@gmail.com>
2021-01-30 19:42:00 +0100

    Refactored function

--- controller/index.ts
+++ controller/index.ts
@@ -11,19 +11,23 @@ const output: IOutput = {
 }
 
 //Add New Data to the database
-export function addNewData(req: Request, res: Response) {
-  const { rule, data }: IBody = req.body;
+export function addNewData(body: IBody) {
+  const {rule, data }=  body;
+
+  // Check if there is no rule or the type of rule is a number
   if (!rule || typeof rule === 'number') {
     output.message = rule ? `rule should be an object.` : `rule is required.`;
     output.status = "error";
     output.data = null;
-    res.status(400).send({...output});
+    return { output, code: 400 };
   }
+
   // Validate the keys in rule
   const defaultKeys = ['field', 'condition', 'condition_value'];
   const defaultTypes = ['string', 'string', 'string or number or object'];
   const ruleKeys = Object.keys(rule);
 
+  // Check the key and it's corresponding type. If it's correct, remove it from defaultKeys else return code 400
   ruleKeys.map(item => {
     if ((item === 'field' && typeof rule[item] === 'string') || (item === 'condition' && typeof rule[item] === 'string') || (item === 'condition_value' && typeof rule[item] !== null)) {
       defaultKeys.splice(defaultKeys.indexOf(item), 1);
@@ -32,17 +36,21 @@ export function addNewData(req: Request, res: Response) {
       output.message = `${item} should be of type ${defaultTypes[defaultKeys.indexOf(item)]}.`;
       output.status = "error";
       output.data = null;
-      res.status(400).send({...output});
+      return { output, code: 400 };
+      // res.status(400).send({ ...output });
     }
+  });
 
-  })
+  // If all the three keys are not present return status 400
   if (ruleKeys.length !== 3 || defaultKeys.length > 0) {
     output.message = `${defaultKeys[0]} is required.`;
     output.status = "error";
     output.data = null;
-    res.status(400).send({...output});
+    return { output, code: 400 };
+    // res.status(400).send({...output});
   }
   
+  // Stringify the entire path to the required field and obtain the value using eval
   const stringifyField = `data.${rule.field}`;
   const value = eval(stringifyField);
 
@@ -50,8 +58,11 @@ export function addNewData(req: Request, res: Response) {
     output.message = `field ${rule.field} is missing from data.`;
     output.status = "error";
     output.data = null;
-    res.status(400).send({...output});
+    return { output, code: 400 };
+    // res.status(400).send({...output});
   }
+
+  // Get present condition and oby=tain a boolean value if the condition is meant or not
   const mainField = rule.field;
   const presentCondition = rule.condition;
   let bool;
@@ -62,7 +73,7 @@ export function addNewData(req: Request, res: Response) {
         presentCondition === '!==' ? value !== rule.condition_value :
           presentCondition === 'contains' ? value.includes(rule.condition_value) : null;
 
-
+  // If the condition is not meant
   if (!bool) {
     output.message = `field ${rule.field} failed validation.`;
     output.status =  "error",
@@ -75,9 +86,11 @@ export function addNewData(req: Request, res: Response) {
         condition_value: rule.condition_value
       }
     }
-    res.status(400).send({...output});
+    // res.status(400).send({...output});
+    return { output, code: 400 };
   }
 
+  // Condition is meant
   output.message = `field ${mainField} successfully validated.`;
   output.status = 'success';
   output.data = {
@@ -89,16 +102,14 @@ export function addNewData(req: Request, res: Response) {
       condition_value: rule.condition_value
     }
   }
-  res.status(200).send({...output});
+  // res.status(200).send({...output});
+  return { output, code: 200 };
 }
 
 //Get all the Data in the database
-export function getAllData(_req: Request, res: Response)  {
-  if (values.length === 0) {
-    return res.status(400).send("No valid field");
-  }
+export function getAllData()  {
   output.message = "My Rule-Validation API";
   output.status = "success";
   output.data = values[0];
-  res.status(200).send(output);
+  return output;
 }
